8fdd63bf6ceaecde99083e61db0180630c105628
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-21 21:53:57 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from marshmallow import Schema, fields
+
+class UserData(Schema):
+    name = fields.Str(required=True)
+    email = fields.Email(required=True)
+    telephonenumbers = fields.Str(required=False)
+    ip = fields.IPv4(required=True)
+
+user_data = {
+    "name": "Vasya",
+    "email": "info@illuminatex.de",
+    "ip": "192.168.0.1"
+}
+
+UserSchema = UserData()
+user = UserSchema.load(user_data)
+
+print(user)
